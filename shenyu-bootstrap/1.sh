b24e215e8530cd784ba6b5db79b2b1cae6961c11
hwy.lll <8d7872b44d3b45c0b79b1017c436d259@example.com>
2024-01-08 16:22:25 +0800

    更新 1.sh

--- shenyu-bootstrap/1.sh
+++ shenyu-bootstrap/1.sh
@@ -5,7 +5,7 @@ for i in `ls`
 do
 if [ -d  $i ];then
    echo $i
-   # cd /home/jenkins/examples/shenyu-bootstrap/overlays/$i
-   # /usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
+   cd /home/jenkins/examples/shenyu-bootstrap/overlays/$i
+   /usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
 fi
 done
