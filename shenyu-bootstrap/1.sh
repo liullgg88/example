e305a8399ea8f3cd35e2ff453e65892d67c3f2ec
Your Name <you@example.com>
2024-01-08 17:11:21 +0800

    xxx

--- shenyu-bootstrap/1.sh
+++ shenyu-bootstrap/1.sh
@@ -5,7 +5,7 @@ for i in `ls`
 do
   if [ -d  $i ];then
      cd /home/jenkins/examples/shenyu-bootstrap/overlays/$i
-     /usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
+     #/usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
      cat kustomization.yaml
   fi
 done
