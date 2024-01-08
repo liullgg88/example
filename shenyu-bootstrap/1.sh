c2c752f150517951dbc369c3ce5499dc82f22932
hwy.lll <8d7872b44d3b45c0b79b1017c436d259@example.com>
2024-01-08 15:21:46 +0800

    更新 1.sh

--- shenyu-bootstrap/1.sh
+++ shenyu-bootstrap/1.sh
@@ -2,5 +2,5 @@
 for i in `ls`
 do 
 cd /home/jenkins/examples/shenyu-bootstrap/overlays/${i}
-/usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
+kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
 done
