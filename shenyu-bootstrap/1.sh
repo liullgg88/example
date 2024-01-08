840a81b404d41256a12a3207f11439aa70080c5a
hwy.lll <8d7872b44d3b45c0b79b1017c436d259@example.com>
2024-01-08 16:00:22 +0800

    更新 1.sh

--- shenyu-bootstrap/1.sh
+++ shenyu-bootstrap/1.sh
@@ -3,8 +3,8 @@ DOCKER_REGISTRY=$1
 VERSION=$2
 for i in `ls`
 do
-    if [ -d  $i ];then
-       cd /home/jenkins/examples/shenyu-bootstrap/overlays/$i
-       /usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
-    fi
+if [ -d  $i ];then
+   cd /home/jenkins/examples/shenyu-bootstrap/overlays/$i
+   /usr/local/bin/kustomize edit set image ${DOCKER_REGISTRY}:${VERSION}
+fi
 done
